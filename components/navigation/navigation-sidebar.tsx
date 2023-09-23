ff9d798acd8f6dafb5fc574ba849f8a05b1d1c7b
malek <malzovich@gmail.com>
2023-09-23 10:31:30 +0100

    feat: Edit page & Channel ID page

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -28,7 +28,7 @@ export const NavigationSidebar = async () => {
   });
   return (
     <div
-      className="space-y-4 flex flex-col items-center h-full text-primary w-full 
+      className="space-y-4 flex flex-col items-center h-full text-primary w-full bg-[#E3E5E8]
   dark:bg-[#1E1F22] py-3"
     >
       <NavigationAction />
